7538fdf126466396fa0ae2198ccba4b981e8da00
FlumHub <mo.attila@proton.me>
2024-04-09 19:13:11 +0200

    testing main changes

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+def main():
+    PATH_TO_FILE = "books/frankenstein.txt"
+    text = get_book_text(PATH_TO_FILE)
+    print(text)
+   
+def get_book_text(path): 
+    with open(path) as f:
+        return f.read()
+main()
